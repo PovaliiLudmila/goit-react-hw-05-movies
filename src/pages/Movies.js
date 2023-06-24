35c042a79cf571d402d237fb9be27126e8b3d7ef
PovaliiLudmila <118770513+PovaliiLudmila@users.noreply.github.com>
2023-06-24 21:49:36 +0300

    12

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,29 +1,60 @@
 import { getQuery } from '../API/Api';
 import { useEffect, useState } from 'react';
 import { useSearchParams, NavLink, useLocation } from 'react-router-dom';
+import { Notify } from 'notiflix';
+Notify.init({ showOnlyTheLastOne: true, clickToClose: true });
 
-const Movies = () => {
+const Movies = ({ isLoading }) => {
   const [movies, setSearchedMovie] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
-  const { query } = searchParams;
   const location = useLocation();
+  const query = searchParams.get('search');
+  const [queryString, setQueryString] = useState(query ? query : '');
 
-  const handleSubmit = e => {
+  function onInputChange(e) {
+    setQueryString(e.target.value);
+  }
+  async function handleSubmit(e) {
     e.preventDefault();
-    const form = e.currentTarget;
-    const newQuery = form.elements.query.value;
-    setSearchParams(newQuery !== '' ? { query: newQuery } : {});
-    form.reset();
-  };
+    setSearchParams(queryString.trim() !== '' ? { search: queryString } : {});
 
+    if (queryString.trim() === '') {
+      Notify.warning('Please enter movie title to search');
+      setSearchedMovie([]);
+    }
+  }
+  // const handleSubmit = e => {
+  //   e.preventDefault();
+  //   const form = e.target;
+  //   const newQuery = form.elements.query.value;
+  //   setSearchParams(newQuery !== '' ? { query: newQuery } : {});
+  //   form.reset();
+  // };
+
+  // useEffect(() => {
+  //   if (!query) return;
+  //   getQuery(query).then(setSearchedMovie);
+  // }, [query]);
   useEffect(() => {
-    if (!query) return;
-    getQuery(query).then(setSearchedMovie);
-  }, [query]);
+    async function getData() {
+      try {
+        if (!query) return;
+        getQuery(query).then(setSearchedMovie);
+        isLoading(true);
+        // const getQuery = await api.searchMovies(query);
+        setSearchedMovie(getQuery.data.results);
+
+        if (getQuery.data.results.length === 0) Notify.failure('Sorry, this movie not found');
+      } finally {
+        isLoading(false);
+      }
+    }
+    getData();
+  }, [query, isLoading]);
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" name="query" defaultValue={query || ''} />
+      <input type="text" value={queryString} onChange={onInputChange} />
       <button type="submit">Search</button>
       {movies.length === 0 && query ? (
         <div>No results. Please try again.</div>
